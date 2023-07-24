1c6cd19852eab217a5ddd1fd5ce37d0d499d065b
Masoud <kingmba.6663@gmail.com>
2023-07-24 11:24:22 +0330

    start project

---
+++ src/main/java/com/example/springsecuritybasice/SpringSecurityBasiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecuritybasice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityBasiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityBasiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springsecuritybasice/SpringSecurityBasiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecuritybasice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityBasiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
