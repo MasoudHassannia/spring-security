70f4e218cba8d5101b38814614696720ae3a33a9
Masoud <kingmba.6663@gmail.com>
2023-07-29 18:20:16 +0330

    create custom Authentication Provider

--- src/main/java/com/example/springsecuritybasic/config/BankUserDetails.java
+++ src/main/java/com/example/springsecuritybasic/config/BankUserDetails.java
@@ -14,10 +14,10 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 
-@Service
+//@Service
 public class BankUserDetails implements UserDetailsService {
 
-    @Autowired
+ //   @Autowired
     private CustomerRepository customerRepository;
 
     @Override
--- src/main/java/com/example/springsecuritybasic/service/CustomerService.java
+++ src/main/java/com/example/springsecuritybasic/service/CustomerService.java
@@ -5,6 +5,8 @@ import com.example.springsecuritybasic.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class CustomerService {
 
@@ -13,5 +15,9 @@ public class CustomerService {
 
     public Customer save(Customer customer){
        return customerRepository.save(customer);
+    }
+
+    public List<Customer> findByEmail(String email){
+        return customerRepository.findByEmail(email);
     }
 }
